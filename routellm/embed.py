202682dfa7a16472c361c797127fef1c7281e1f4
HyunjoonCho <jhj09016@gmail.com>
2025-05-09 16:55:46 +0900

    prepare trimmed prompts for embedding

--- routellm/embed.py
+++ routellm/embed.py
@@ -2,12 +2,12 @@ import json
 from routers.matrix_factorization.request_wrapper import get_embeddings
 
 results_path = '../AutoFL/results/d4j_autofl_eol_1/llama3'
-initial_prompts_path = 'route_data/initial_prompts.json'
-embeddings_path = 'route_data/embeddings.json'
+prompts_path = 'route_data/trimmed_prompts.json'
+embeddings_path = 'route_data/trimmed_embeddings.json'
 
 if __name__ == "__main__":
     embeddings_dict = dict()
-    with open(initial_prompts_path) as f:
+    with open(prompts_path) as f:
         data = json.load(f)
         
     for bug_id in data:
